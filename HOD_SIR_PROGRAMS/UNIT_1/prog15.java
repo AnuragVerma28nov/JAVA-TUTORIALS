158f42a29b849995fa69a67da5ca005b65efc969
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-04-21 22:15:58 +0530

    Add files via upload

---
+++ HOD_SIR_PROGRAMS/UNIT_1/prog15.java
@@ -0,0 +1,18 @@
+//  Print First N Sum Numbers
+
+import java.util.*;
+
+public class prog15 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first " + n + " numbers: " + sum);
+        // sc.close();
+    }
+}
---
+++ HOD_SIR_PROGRAMS/UNIT_1/prog16.java
@@ -0,0 +1,20 @@
+//Check if a Number is Negative, Positive, or Zero
+
+import java.util.Scanner;
+
+public class prog16 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println("Number is zero.");
+        }
+        // sc.close();
+    }
+}
