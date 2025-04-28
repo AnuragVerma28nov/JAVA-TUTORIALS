7a05b18c24455978e69410d1e39cf6354303d804
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-04-28 23:59:03 +0530

    Add files via upload

---
+++ HOD_SIR_PROGRAMS/UNIT_1/prog24.java
@@ -0,0 +1,15 @@
+// reading a string
+
+import java.util.*;
+
+public class prog24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String s = sc.nextLine();
+        System.out.println("Your name is " + s+".");
+
+        // sc.close();
+    }
+}
---
+++ HOD_SIR_PROGRAMS/UNIT_1/prog27.java
@@ -0,0 +1,11 @@
+// command line argument
+
+public class prog27 {
+
+    public static void main(String[] args) {
+        System.out.print("Enter your string: ");
+        System.out.println("Your frist argument: " + args[0]);
+        System.out.println("Your frist argument: " + args[1]);
+        // sc.close();
+    }
+}
