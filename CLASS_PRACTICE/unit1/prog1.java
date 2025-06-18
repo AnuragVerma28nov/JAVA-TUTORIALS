8a276c8b89146d4920c872df79ace583f718ac8d
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-18 23:57:00 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/unit1/prog1.java
@@ -0,0 +1,11 @@
+// my frist java program
+
+
+public class prog1 {
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("x=" + x);
+        System.out.println("Hello, World!");
+    }
+}
