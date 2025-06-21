b227c1bdf9d3289f04f2f684aa6e80b46ede8c02
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-21 22:58:58 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/unit1/UNIT_1/prog2.java
@@ -0,0 +1,11 @@
+ //creating object
+ 
+ class prog2 {
+
+    int x = 10;
+
+    public static void main(String[] args) {
+        prog2 obj = new prog2();  //creating object
+        System.out.println(obj.x);
+    }
+}
