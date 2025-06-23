50d4d2ffcd31fb0b71c3d02a29881892ceb2f4a9
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-23 22:48:40 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/UNIT_1/prog18.java
@@ -0,0 +1,25 @@
+// checking palindrome number
+
+import java.util.Scanner;
+
+public class prog18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int n = num;
+        int d, r = 0;
+        while (n > 0) {
+            d = n % 10;
+            r = r * 10 + d;
+            n = n / 10;
+        }
+        if (num == r) {
+            System.out.println(num + " is palindrome number.");
+        } else {
+            System.out.println(num + " is not palindrome number.");
+        }
+        sc.close(); 
+    }
+}
