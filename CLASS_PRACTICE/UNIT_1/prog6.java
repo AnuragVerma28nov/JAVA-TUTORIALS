7681a674058ca6eaf9a3a8c5222142cd971eb442
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-26 00:20:46 +0530

    Update prog6.java

---
+++ CLASS_PRACTICE/UNIT_1/prog6.java
@@ -0,0 +1,19 @@
+// use of private method in subclass pack 
+// creating object of subclass pack in main class prog6 and calling private method
+
+class pack {
+    private void display() {
+        System.out.println("hello World!");
+
+    }
+}
+
+public class prog6 {
+    public static void main(String[] args) {
+        pack obj = new pack();
+        obj.display(); 
+    }
+
+}
+// Output:
+// shows compilation error because display() is private in pack class
