7cfbe9d470ad6184c43d5911360b61e9c79f69a6
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-26 00:21:13 +0530

    Update prog7.java

---
+++ CLASS_PRACTICE/UNIT_1/prog7.java
@@ -0,0 +1,18 @@
+// use of protected method in subclass pack 
+// creating object of subclass pack in main class prog6 and calling protected method
+
+class pack {
+    protected void display() {
+        System.out.println("hello World!");
+
+    }
+}
+
+public class prog7 {
+    public static void main(String[] args) {
+        pack obj = new pack();
+        obj.display(); // calling protected method using object
+    }
+
+}
+// Output: hello World!
