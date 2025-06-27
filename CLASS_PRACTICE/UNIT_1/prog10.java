653452327d9031b3b8bbd807d76d4750fc60556d
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-27 19:25:31 +0530

    Update prog10.java

---
+++ CLASS_PRACTICE/UNIT_1/prog10.java
@@ -0,0 +1,25 @@
+// use of final class in a subclass CSE 
+// creating object of subclass CSE in main class prog10 and calling final method
+
+final class CSE {
+    void display() {
+        System.out.println("This is a final class in class CSE.");
+
+    }
+}
+
+public class prog10 extends CSE {
+    void display() {
+        System.out.println("This is a final method in class prog10.");
+
+    }
+
+    public static void main(String[] args) {
+        CSE obj = new CSE(); // creating object of CSE
+        obj.display(); // calling final method using object
+
+    }
+}
+//output:shows compilation error: Cannot inherit from final 'CSE'
+// Note: The class CSE is declared as final, which means it cannot be subclassed.
+
