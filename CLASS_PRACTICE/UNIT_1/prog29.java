0b93e75de5467fad5f492a4986bb2c3fd9b50f09
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-06-30 14:45:38 +0530

    Update prog29.java

--- CLASS_PRACTICE/UNIT_1/prog29.java
+++ CLASS_PRACTICE/UNIT_1/prog29.java
@@ -1,4 +1,6 @@
-
+// oops-polymorphism
+// polymorphism is the ability of a single function or method to operate in different ways based on the input parameters or the context in which it is called.
+//In Java, polymorphism can be achieved through method overloading and method overriding.
 class Area {
 
     void area(int n) {
@@ -21,3 +23,7 @@ public class prog29 {
         obj.area(5, 5);
     }
 }
+
+// Output:
+// Area of Square: 25
+// Area of Reactanlge: 25
