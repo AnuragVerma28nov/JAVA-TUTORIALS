aabb808e8aa068c09edf8249f070ca7ec450712d
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-04 22:00:40 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/UNIT_2/prog1.java
@@ -0,0 +1,14 @@
+package UNIT_2;
+
+public class prog1 {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b; // This will throw ArithmeticException
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException caught: Division by zero is not allowed.");
+        }
+    }
+}
