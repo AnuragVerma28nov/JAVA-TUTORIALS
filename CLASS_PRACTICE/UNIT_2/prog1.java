89b432dbeae2a56a6de8c33238e8ac2e3882e768
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-05 18:42:57 +0530

    Update prog1.java

--- CLASS_PRACTICE/UNIT_2/prog1.java
+++ CLASS_PRACTICE/UNIT_2/prog1.java
@@ -6,11 +6,13 @@ public class prog1 {
     
     public static void main(String[] args) {
         try {
+            System.out.println("Inside try block.");
             int a = 10;
             int b = 0;
             int result = a / b; // This will throw ArithmeticException
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
+            System.out.println("Inside catch block.");
             System.out.println("ArithmeticException caught: Division by zero is not allowed.");
         }
         finally {
@@ -20,6 +22,8 @@ public class prog1 {
     }
 }
 // Output:
+// Inside try block.
+// Inside catch block.
 // ArithmeticException caught: Division by zero is not allowed.
 // This finally block always executes.
 // Rest of the code.
