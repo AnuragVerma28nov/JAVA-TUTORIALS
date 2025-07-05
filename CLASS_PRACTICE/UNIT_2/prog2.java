221b0e29a1ef39ce700f1ceadd0615cf6814d91e
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-05 18:41:21 +0530

    Update prog2.java

--- CLASS_PRACTICE/UNIT_2/prog2.java
+++ CLASS_PRACTICE/UNIT_2/prog2.java
@@ -5,10 +5,12 @@ package UNIT_2;
 public class prog2 {
     
     public static void main(String[] args) {
+        int[] arr = new int[5]; // Array of size 5
         try {
-            int[] numbers = {1, 2, 3};
-            System.out.println(numbers[5]); 
+            System.out.println("Inside try block.");
+            arr[10]=10; // This will throw ArrayIndexOutOfBoundsException
         } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Inside catch block.");
             System.out.println("ArrayIndexOutOfBoundsException caught: Invalid array index.");
         }
         finally {
@@ -18,6 +20,8 @@ public class prog2 {
     }
 }
 // Output:
+// Inside try block.
+// Inside catch block.
 // ArrayIndexOutOfBoundsException caught: Invalid array index.
 // This finally block always executes.
 // Rest of code. Program continues...
