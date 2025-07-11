e811424706aba3725e89dc1237e277f3434a2ce0
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-11 22:45:55 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/UNIT_3/prog11.java
@@ -0,0 +1,23 @@
+// for each loop 
+
+package UNIT_3;
+
+import java.util.*;
+
+public class prog11 {
+    public static void main(String[] args) {
+        List<Integer> num = new ArrayList<Integer>();
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        num.forEach(n -> System.out.println(n));
+    }
+    
+}
+
+// Output:
+// 1
+// 2
+// 3
+// 4
