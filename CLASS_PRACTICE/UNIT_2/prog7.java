331aa7b2744e96d1ba3b85bff09aa9256a5d3711
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-12 22:03:36 +0530

    Update prog7.java

--- CLASS_PRACTICE/UNIT_2/prog7.java
+++ CLASS_PRACTICE/UNIT_2/prog7.java
@@ -3,7 +3,7 @@
 package UNIT_2;
 
 public class prog7 {
-    public static void checkNum(int num) throws IllegalArgumentException { // Method to check if the number is positive
+    public  void checkNum(int num) throws IllegalArgumentException { // Method to check if the number is positive
         if (num < 0) {
             throw new IllegalArgumentException("Number is negative: " + num);
         } else {
@@ -12,11 +12,19 @@ public class prog7 {
     }
 
     public static void main(String[] args) {
-        prog7.checkNum(-5); // This will throw an exception
+        prog7 prog7 = new prog7();
+        try {
+            prog7.checkNum(10); // This will not throw an exception
+            prog7.checkNum(-5); // This will throw an exception
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
         System.out.println("Rest of code. Program continues...");
        
     }
 }
 
 // Output:
-// Exception in thread "main" java.lang.IllegalArgumentException: Number is negative: -5
+// Number is positive: 10
+// Exception caught: Number is negative: -5
+// Rest of code. Program continues...
