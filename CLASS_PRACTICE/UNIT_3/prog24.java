306c710b2ac608ac89244b1caf5a49250548c275
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-07-13 15:14:52 +0530

    Add files via upload

---
+++ CLASS_PRACTICE/UNIT_3/prog24.java
@@ -0,0 +1,28 @@
+// Record in Java Example
+
+package UNIT_3;
+
+public class prog24 {
+
+    public static void main(String[] args) {
+        String record = """
+        Record ID: 2304220100001
+        Name: Anurag Verma
+        Age: 20
+        Address: BIET Hostel Lucknow, Uttar Pradesh, India
+        Phone: 914077wxyz
+        Email: anurag.verma@example.com
+        
+        """;
+        System.out.println(record);
+    }
+}
+
+// Output:
+// Record ID: 2304220100001
+// Name: Anurag Verma
+// Age: 20
+// Address: BIET Hostel Lucknow, Uttar Pradesh, India
+// Phone: 914077wxyz
+// Email: anurag.verma@example.com
+// Address: 123 Main St, Anytown, USA
