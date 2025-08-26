e376d99dac4ceeb534aba544d96625acfc929f5c
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-08-26 19:18:42 +0530

    Add files via upload

---
+++ SELF_STUDY_MATERIALS/CH_1/NameGreet.java
@@ -0,0 +1,14 @@
+package chap2;
+
+import java.util.Scanner;
+
+public class NameGreet {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.err.println("Hello!" + name + ".Have a nice day!");
+        sc.close();
+    }
+}
