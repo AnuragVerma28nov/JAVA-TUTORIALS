22e1c1ee886722f162c4ec3c4d9503a5ee52fa40
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-09-19 22:53:42 +0530

    Update prog1.java

--- CLASS_PRACTICE/UNIT_1/prog1.java
+++ CLASS_PRACTICE/UNIT_1/prog1.java
@@ -5,7 +5,7 @@ public class prog1 {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        System.out.println("I am ANURA VERMA.");
+        System.out.println("I am ANURAG VERMA.");
         System.out.println("This is my first Java program.");
         System.out.println("I am learning Java programming.");
         System.out.println("Java is a versatile language.");
@@ -20,4 +20,5 @@ public class prog1 {
 // I am learning Java programming.
 // Java is a versatile language.
 // I am excited to learn more about Java.
+
 
