344f38e5da0fcc5f53c471c881ac736ec47f5b92
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-10-01 20:13:28 +0530

    Add files via upload

---
+++ STUDY_MATERIALS/CH_1/VariableImplementation.java
@@ -0,0 +1,28 @@
+// Variable Implementation
+
+package chap2;
+
+public class VariableImplementation {
+
+    public static void main(String[] args) {
+
+        byte age = 50;
+        int n = 60;
+        short m = 60;
+        long o = 340;
+        char ch = 'a';
+        float f = 5.2f;
+        double d = 56d;
+        boolean b = true;
+
+        System.out.println("Byte " + age);
+        System.out.println("Int " + n);
+        System.out.println("Short " + m);
+        System.out.println("Long " + o);
+        System.out.println("char " + ch);
+        System.out.println("Float "+f);
+        System.out.println("Double " +d);
+        System.out.println("Boolean "+b);
+
+    }
+}
