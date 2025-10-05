b89612a7e2016ec8b2f4e9d2a9731fc6a63b6101
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-10-05 16:48:46 +0530

    first commit

---
+++ PW_CLASSES/FloatDataTypes.java
@@ -0,0 +1,13 @@
+package PW_CLASSES;
+
+public class FloatDataTypes {
+    public static void main(String[] args) {
+        // Declaration and initialization of float and double
+        float f = 12.56f;     // 'f' or 'F' is required for float literals
+        double d = 45.78965;  // Default type for decimals is double
+
+        // Display values
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+    }
+}
