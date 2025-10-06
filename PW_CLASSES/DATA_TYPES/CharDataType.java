7bc3673aaff4f1f29fc51d0a01c7a8fcf51a56a6
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-10-07 01:15:40 +0530

    data types added

---
+++ PW_CLASSES/DATA_TYPES/CharDataType.java
@@ -0,0 +1,13 @@
+public class CharDataType {
+    public static void main(String[] args) {
+        // Declaration and initialization of char variables
+        char letter = 'A';
+        char digit = '9';
+        char symbol = '#';
+
+        // Display the characters
+        System.out.println("Letter: " + letter);
+        System.out.println("Digit: " + digit);
+        System.out.println("Symbol: " + symbol);
+    }
+}
